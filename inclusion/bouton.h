ccb58edb66471b4fcab23e07a559d8eabbe511e6
benoit <benoit-42820@hotmail.fr>
2018-02-03 18:38:45 -0500

    Réarchitecture de l'arborescence du projet. Création d'un dossier d'entêtes.

--- inclusion/bouton.h
+++ inclusion/bouton.h
@@ -3,14 +3,12 @@
 #include <string>
 #include <SFML/Graphics.hpp>
 
-#include "bouton.h"
-
 class Bouton : public sf::RectangleShape{
 	private:
 		int bordure;
 		sf::Text contenu;
 	public:
-		Bouton(int hauteur, int longueur, std::string texte, sf::Font police);
+		Bouton(int hauteur, int longueur, std::string& texte, sf::Font& police);
 		sf::Vector2f getPosition();
 		void setPosition(float x, float y);
 };
--- inclusion/menu.h
+++ inclusion/menu.h
@@ -2,11 +2,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#include "bouton.h"
+
 class Menu : public sf::RenderWindow{
 	private:
-		sf::RectangleShape btnHeberger;
-		sf::RectangleShape btnJoindre;
-		sf::RectangleShape btnParametres;
+		Bouton * btnHeberger;
+		Bouton * btnJoindre;
+		Bouton * btnParametres;
 		sf::Text titre;
 	public:
 		Menu();
