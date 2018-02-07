e1ce39f4be3752dbcbc7f9b973ae69954567b5ef
benoit <benoit-42820@hotmail.fr>
2018-02-07 18:08:32 -0500

    Ajout des mots clés "override" à la définition de la classe. references #5 #6

--- inclusion/bouton.h
+++ inclusion/bouton.h
@@ -15,7 +15,7 @@ class Bouton : public sf::RectangleShape{
 	public:
 		Bouton(int hauteur, int longueur, std::string texte, const std::string& cheminPolice);
 		void setBtnPosition(float x, float y);
-		const sf::Vector2f getPosition();
-		const sf::Vector2f getSize();
+		const sf::Vector2f getPosition() override;
+		const sf::Vector2f getSize() override;
 		
 };
