af48696a08992c0de7a2df07095897f5171ffd2c
Nicolas <nicolas.neila@etu.univ-lyon1.fr>
2018-02-09 15:09:15 -0500

    Suppression de fichiers inutiles

--- src/views/panneau.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <string>
-#include "../../inclusion/panneau.h"
-
-Panneau::Panneau(int longueurPanneau, int hauteurPanneau, sf::Texture texturePanneau) :
-	longueur(longueurPanneau), hauteur(hauteurPanneau), texture(texturePanneau)
-{
-
-}
