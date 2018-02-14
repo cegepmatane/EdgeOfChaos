8ab8b8cc94bbe015742f6fe862c79f29a8340215
benoit <benoit-42820@hotmail.fr>
2018-02-14 16:40:18 -0500

    Ajout du prototype du programme principal.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,76 +1,35 @@
-#include "../inclusion/FenetreJeu.h"
+#include <iostream>
+#include <string>
 
-int main()
+#include "../inclusion/menu.h"
+
+
+int main(void)
 {
-	/* Définitions des mesures */
+	Menu menu;
+	bool estConnecte = true;
+	std::string tst = "Pas de connexion reseau.";
 
-	int longueurFenetre = 1280;
-	int hauteurFenetre = 768;
-	std::string nomFenetre = "Edge of Chaos";
-
-	int tailleCase = 64;
-
-	int hauteurNiveau = 20;
-	int hauteurGrille = 9;
-	int hauteurPanneau = 3;
-
-	int longueurNiveau = 40;
-	int longueurGrille = 20;
-
-	/* Déclaration du niveau et des textures */
-
-	int niveau[] =
+	while(menu.isOpen())
 	{
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 4, 4, 4, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 2, 2, 4, 4, 4, 2, 2, 4, 4, 4, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 2, 2, 4, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-		0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 2, 2, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0,
-		0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 2, 2, 2, 4, 4, 4, 2, 2, 1, 0, 0, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
-		0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 1, 0, 0, 1, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0,
-		2, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0,
-		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 0, 0, 0,
-		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0, 0, 0,
-		4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	};
-	std::string imagePanneau = "ressources/textures/bois2.jpg";
-
-	/* Déclaration des unités et bâtiments */
-
-	int attaque = 10;
-	std::vector<std::string> listeAttaques;
-	listeAttaques.push_back("Charge");
-	listeAttaques.push_back("Chun chun maru!");
-	Unite unite("Test", 100, tailleCase*16, tailleCase*14, attaque, 10, listeAttaques);
-	unite.setImage("ressources/textures/textures.png", 8);
-
-	std::vector<std::string> optionsBatiment;
-	optionsBatiment.push_back("Créer un lancier");
-	optionsBatiment.push_back("Créer un homme d'arme");
-	Batiment batiment("Caserne", 100, tailleCase * 26, tailleCase * 9, "Vous pouvez y former des unités", optionsBatiment);
-	batiment.setImage("ressources/textures/textures.png", 9);
-
-	std::vector<Unite> unites;
-	std::vector<Batiment> batiments;
-
-	unites.push_back(unite);
-	batiments.push_back(batiment);
-
-	/* Appel de la fenêtre de jeu */
-
-	FenetreJeu fenetre(longueurFenetre, hauteurFenetre, nomFenetre, longueurNiveau, hauteurNiveau, longueurGrille, hauteurGrille,
-		tailleCase, hauteurPanneau, niveau, imagePanneau, unites, batiments);
-	
-	fenetre.lancerBoucle();
+		sf::Event event;
+		while(menu.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				// Inclusion de l'initialisation du jeu ici
+				menu.initJeu();
+				// // // // // // //
+				menu.close();
+			}
+				menu.clear(sf::Color::Yellow);
+				menu.afficher();
+				if(!estConnecte){
+					menu.lancerErreur(tst);
+				}
+				menu.display();
+		}
+	}
 
 	return 0;
 }
